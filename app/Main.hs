c0e08b0052238e36261f4efef37a32065888a78c
Salih Uysal <esaeliha@gmail.com>
2020-05-12 17:24:08 +0200

    Added Euler Function (ggT and ggT') and tests

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,11 @@ import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
 
 main :: IO ()
 main = do
-hSetBuffering stdout NoBuffering
-putStr "Geben Sie bitte eine Zahl ein (0 == Ende): "
-number <- readLn :: IO Double
-if number == 0
-  then putStrLn "Ciao"
-  else do 
-    putStrLn ("square(" ++ show number ++ ") = " ++ show (square number))
-    main
+  hSetBuffering stdout NoBuffering
+  putStr "Geben Sie bitte eine Zahl ein (0 == Ende): "
+  number <- readLn :: IO Double
+  if number == 0
+    then putStrLn "Ciao"
+    else do 
+      putStrLn ("square(" ++ show number ++ ") = " ++ show (square number))
+      main
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,20 @@
-module Lib (someFunc, square) where
+-- / A Lib Module.
+module Lib (someFunc, square, ggT, ggT') where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-square :: Double -> Double
-square n = n * n
+square 
+  :: Num a => a -- ^ the number
+  -> a -- ^ the square
+square n = n^2
+
+ggT :: Integer -> Integer -> Integer
+ggT a b
+      | b == 0    = abs a
+      | otherwise = ggT b (a `mod` b)
+
+ggT' :: Integer -> Integer -> Integer
+ggT' a b = if b == 0
+            then abs a
+            else ggT' b (a `mod` b)
